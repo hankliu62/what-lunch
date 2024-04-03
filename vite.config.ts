d9bb4b639ae68d5cfd7aa9e62180bb9b6c16f58b
hankliu <397694072@qq.com>
2024-04-04 01:10:35 +0800

    feat(vite): use vite plugin autobase

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { UserConfig, defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import autobase from "./lib/autobase.umd";
 
 const config: UserConfig = {
-  plugins: [vue()],
+  plugins: [vue(), autobase()],
 
   define: {
     "process.env": {
@@ -10,19 +11,6 @@ const config: UserConfig = {
     },
   },
 };
-
-// 是否通过github actions部署
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-  // 用于为静态资源（如图像、样式表、JavaScript 文件等）设置 URL 前缀
-  // 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
-  config.base = `/${repo}/`;
-  config.define["process.env"]["ROUTE_PREFIX"] = `/${repo}`;
-
-  console.log("nextConfig:", config);
-}
 
 // https://vitejs.dev/config/
 export default defineConfig(config);
