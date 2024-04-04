98c5129f32cb77f7080b2d3d3e8e9f4bf7420399
hankliu <397694072@qq.com>
2024-04-05 00:04:34 +0800

    feat(vite): add plugin to deal prefix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { UserConfig, defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import autobase from "./lib/autobase.umd";
+import autobase from "vite-plugin-autobase";
 
 const config: UserConfig = {
   plugins: [vue(), autobase()],
